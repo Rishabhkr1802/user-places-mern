44426210192352159957d00aeaf12964eb4333b1
rishabhkr1802 <krrishabh1802@gmail.com>
2025-07-03 19:33:19 +0530

    Add Sidebar component

--- Frontend/src/sharedComponents/Layout/Layout.jsx
+++ Frontend/src/sharedComponents/Layout/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../Sidebar/Sidebar";
 
 function Layout() {
     return (
         <div className="row">
-            <div className="col-4">Sidebar</div>
+            <div className="col-4"><Sidebar /></div>
             <div className="col-8"><Outlet /></div>
         </div>
     );
---
+++ Frontend/src/sharedComponents/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <>
+      I'm a Sidebar component!!!
+    </>
+  )
+}
+
+export default Sidebar;
