5e92755f4980fbda86e64848e444222debaa2e6d
rishabhkr1802 <krrishabh1802@gmail.com>
2025-07-04 21:12:37 +0530

    Fix screen issue

--- Frontend/src/routes/Router.jsx
+++ Frontend/src/routes/Router.jsx
@@ -2,15 +2,15 @@ import { lazy, Suspense } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const Dashboard = lazy(() => import('../components/Dashboard/Dashboard'));
-const Layout = lazy(() => import('../sharedComponents/Layout/Layout'));
+const Layout    = lazy(() => import('../sharedComponents/Layout/Layout'));
 
 const router = createBrowserRouter([
     {
-        path: '/', element: Layout,
+        path: '/', element: <Layout />,
         children: [
             { index: true, element: <Dashboard /> }
         ]
-    }
+    },
 ]);
 
 function Router() {
